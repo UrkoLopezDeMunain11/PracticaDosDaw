c1df072207d745cc25e1e867d55ac693bd060d95
Lier <lier.garrido.47469@ikasle.egibide.org>
2025-10-29 11:42:26 +0100

    commit de la rama uno

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String nombreCliente = JOptionPane.showInputDialog("Escribe el nombre del cliente:");
+        List<Producto> productos = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            String nombreProducto = JOptionPane.showInputDialog("Escribe el nombre del producto:");
+            if (nombreProducto == null || nombreProducto.isEmpty()) {
+                break;
+            }
+
+            int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Escribe la cantidad de " + nombreProducto + ":"));
+            double precioUnitario = Double.parseDouble(JOptionPane.showInputDialog("Escribe el precio unitario de " + nombreProducto + ":"));
+
+            String[] opcionesIVA = {"21%", "10%", "4%"};
+            int opcionIVA = JOptionPane.showOptionDialog(null, "Seleccione el tipo de IVA:", "IVA",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcionesIVA, opcionesIVA[0]);
+
+            double iva = 0.21;
+            if (opcionIVA == 1) iva = 0.10;
+            else if (opcionIVA == 2) iva = 0.04;
+
+            productos.add(new Producto(nombreProducto, cantidad, precioUnitario, iva));
         }
     }
-}
